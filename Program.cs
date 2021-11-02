a5d637a278671470b31b952771f697bf5ed128b8
Ameejac <ameejacherukuru123@gmail.com>
2021-11-02 16:18:19 +0530

    ameeja [added: equal and compTo methods by using oops concept]

--- Program.cs
+++ Program.cs
@@ -4,7 +4,9 @@ namespace ConsoleApp2
 {
     class Program
     {
-        //uc1 calculating line length
+        
+       
+        //uc4 equal and compareTo method by oops concept 
        static double Distance(int X1,int Y1,int X2,int Y2)
         {
             //length. - A Length as 2 Points (x1, y1) and (x2, y2)
@@ -14,26 +16,33 @@ namespace ConsoleApp2
         }
         static void Main(string[] args)
         {
-	    //uc2 Equality method
+            // equality method
             double line1 = Distance(1, 3, 4, 5);
             double line2 = Distance(1, 3, 4, 5);
-
-	    //uc3 Compareto Method
-            int compTo = line1.CompareTo(line2);
-            Console.WriteLine("compareto:" + compTo);
-            if(compTo > 0)
+            if (line1 == line2)
             {
-                Console.WriteLine("length is greater");
-            }
-            else if(compTo < 0)
-            {
-                Console.WriteLine("length is lesser");
+                Console.WriteLine("length is prefferable ");
             }
             else
             {
+                Console.WriteLine("length is not prefferable");
+            }
+            //compareTo method
+            int compTo = line1.CompareTo(line2);
+            Console.WriteLine("compareto:" + compTo);
+            if (compTo > 0)
+            {
+                Console.WriteLine("length is greater");
+            }
+            else if (compTo < 0)
+            {
+                Console.WriteLine("length is lesser");
+            }
+            else 
+            {
                 Console.WriteLine("length is equal");
             }
-            Console.WriteLine("length of line is :"+line1);
+            Console.WriteLine("length of first  line is :"+line1);
             Console.WriteLine("length of second line:" + line2);
 
 
